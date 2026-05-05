4a1b8ec81af886e6a46a33f8842d13171877c81a
Ankur Mittal <ankurmittal9081@gmail.com>
2026-05-05 10:01:52 +0530

    Initial commit

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
     @GetMapping("/")
     public  String hello(){
-        return  "hello ankur";
+        return  "hello ankur mittal";
     }
 
 }
